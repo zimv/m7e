848f3ba723ba3626ed542a7d99d4431089928f2e
mayiwen <mayiwen@medlinker.com>
2021-08-31 00:49:44 +0800

    feat: moca-2 update

--- components/moca-card/index.tsx
+++ components/moca-card/index.tsx
@@ -7,10 +7,10 @@ interface Props {
   className?: string;
   avatar: StaticImageData;
   name: string;
-  description: string;
+  text: string;
 }
 
-export default function MocaCard({ avatar, name, description, className, ...props }: Props) {
+export default function MocaCard({ avatar, text, className, ...props }: Props) {
   const cls = classnames(styles['moca-card'], className);
   return (
     <div {...props} className={cls}>
@@ -19,7 +19,9 @@ export default function MocaCard({ avatar, name, description, className, ...prop
         <span className={styles.num}>102</span>
       </div>
       <div className={styles.body}>
-        <div className={styles.tit}></div>
+        <div className={styles.tit}>
+          <div className={styles.name}>Self Awakened</div>
+        </div>
         <div className={styles.prod}>
           <Image src={avatar} alt="production" />
         </div>
@@ -28,11 +30,7 @@ export default function MocaCard({ avatar, name, description, className, ...prop
         <div className={styles.l}>medlo</div>
         <div className={styles.r}>divoq</div>
       </div>
-      <div className={styles.footer}>
-        Time Currents is created amid musings: life unfolds itself in a patterned manner, yet we
-        hold the power to choose how to ride waves of time. The pixels, which represent time units,
-        are liquified and reborn into new shades and shapes.
-      </div>
+      <div className={styles.footer}>{text}</div>
     </div>
   );
 }
