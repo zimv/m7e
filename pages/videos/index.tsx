40badb7dc22868378725339c4d127c2937d3efca
shiqiang <shiqiang@edmodo.com>
2021-09-05 15:10:43 +0800

    feat: add video page

--- pages/videos/index.tsx
+++ pages/videos/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import classnames from 'classnames';
-// import 'react-responsive-carousel/lib/styles/carousel.min.css';
-// import { Carousel } from 'react-responsive-carousel';
 import { Carousel } from 'antd';
 import ReactPlayer from 'react-player';
 import Bg1 from '../../public/images/videos_bg_1.svg';
