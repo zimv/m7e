63601682e9ec27d23b07680a2a4ac669ce47a782
godotdotdot <chukui0202@gmail.com>
2021-09-24 01:06:18 +0800

    feat: update

--- common/const.ts
+++ common/const.ts
@@ -1,6 +1,6 @@
 export const NAVIGATION = [];
 export const SITE_NAME_MULTILINE = ['NFT', '4', 'Metaverse'];
-export const SITE_NAME = 'NFT4Metaverse';
-export const SITE_URL = 'https://nft4metaverse.io';
+export const SITE_NAME = 'Shanghai Metaverse Week 2021';
+export const SITE_URL = 'https://m7e.sh';
 export const META_DESCRIPTION = 'Shanghai Metaverse Week 2021';
 export const SITE_DESCRIPTION = 'Shanghai Metaverse Week 2021';
