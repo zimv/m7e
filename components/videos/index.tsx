a4e88a489512d047c2b007fb990961d2df4c6e39
shiqiang <shiqiang@nd.com.cn>
2021-10-09 22:56:42 +0800

    fix: update highlights

--- components/videos/index.tsx
+++ components/videos/index.tsx
@@ -17,23 +17,44 @@ const videoList = [
       'https://bee-6.gateway.ethswarm.org/bzz/9c0e0d6cbda6a166ac414dc9ebb905a10951cd97fa75b6dd7c364a3aa0c9772d',
     image:
       'https://images.unsplash.com/photo-1614983646436-b3d7a8398b3f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNTU4MTkxNA&ixlib=rb-1.2.1&q=80&w=400&h=600',
-    title: 'Colborn',
+    title: 'Shanghai Metaverse Week',
+    description:
+      'Self-sovereign identities and avatars are the prerequisites and first-class assets for people to enter the metaverse to work, live and play freely in future…',
+  },
+  {
+    video:
+      'http://1500006270.vod2.myqcloud.com/6c9ac477vodcq1500006270/9c51ac823701925925598037101/playlist.f9.mp4',
+    title: "What's the magic between data and NFT?",
+    description:
+      "@JESSCATE93 @meta_Synth will share their insights and showcase Dataverse, a new #NFT curation product developed by @OwnershipLabs. Let's enjoy the 2nd talkshow on @binance live host by @cryptoroaming tonight for #ShanghaiMetaverseWeek 2021",
+  },
+  {
+    video:
+      'http://1500006270.vod2.myqcloud.com/6c9ac477vodcq1500006270/08f0c0fa3701925925300544730/playlist.f5.mp4',
+    title: 'What kind of infrastructure will the Metaverse need in the future?',
     description: 'MOCA',
   },
-  // {
-  //   video: '/video/moca.mp4',
-  //   image:
-  //     'https://images.unsplash.com/photo-1615421559287-5e6eecec3b80?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNTU4MjAwOQ&ixlib=rb-1.2.1&q=80&w=400&h=600',
-  //   title: 'Suji',
-  //   description: 'MASK',
-  // },
-  // {
-  //   video: '/video/moca.mp4',
-  //   image:
-  //     'https://images.unsplash.com/photo-1615098270177-e2db45986811?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNTU4MjAwOQ&ixlib=rb-1.2.1&q=80&w=400&h=600',
-  //   title: 'Jean Chen',
-  //   description: 'NFT4Metaverse',
-  // },
+  {
+    video:
+      'http://1500006270.vod2.myqcloud.com/6c9ac477vodcq1500006270/14a7ff843701925924372545542/playlist.f9.mp4',
+    title: '自我的觉醒',
+    description:
+      "Welcome to @binance live in 30 minutes to learn what #Metaverse is and what #ShanghaiMetaverseWeek 2021 will offer. Let's scan the QR code to enjoy the interview with @cryptoroaming by @kaylawangnow CMO of @litentry @Web3Mcp!",
+  },
+  {
+    video: '/video/moca.mp4',
+    image:
+      'https://images.unsplash.com/photo-1615421559287-5e6eecec3b80?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNTU4MjAwOQ&ixlib=rb-1.2.1&q=80&w=400&h=600',
+    title: 'Suji',
+    description: 'MASK',
+  },
+  {
+    video: '/video/moca.mp4',
+    image:
+      'https://images.unsplash.com/photo-1615098270177-e2db45986811?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNTU4MjAwOQ&ixlib=rb-1.2.1&q=80&w=400&h=600',
+    title: 'Jean Chen',
+    description: 'NFT4Metaverse',
+  },
 ];
 
 export default function Videos({ userAgent }) {
@@ -53,22 +74,18 @@ export default function Videos({ userAgent }) {
           <Bg1 className={styles.bg1} />
           <div className={shdowCls}>
             <Carousel className={styles.main} dotPosition="top" dots={{ className: styles.dot }}>
-              {videoList.map((item) => {
-                return (
-                  <div className={styles.video}>
-                    <ReactPlayer
-                      controls
-                      loop
-                      playing
-                      muted
-                      width="100%"
-                      height="100%"
-                      url={item.video}
-                      className="w-full h-full rounded-2xl overflow-hidden"
-                    />
-                  </div>
-                );
-              })}
+              <div className={styles.video}>
+                <ReactPlayer
+                  controls
+                  loop
+                  playing
+                  muted
+                  width="100%"
+                  height="100%"
+                  url={''}
+                  className="w-full h-full rounded-2xl overflow-hidden"
+                />
+              </div>
             </Carousel>
             <div className="flex flex-col text-left w-full">
               <div className={styles.title}>Shanghai Metaverse Week</div>
